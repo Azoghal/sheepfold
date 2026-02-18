0a97533bb71af13a6d99c30a39d8f76fcf7477a1
Sam Beer <sam.beer208@gmail.com>
2026-02-18 20:40:10 +0000

    timer can speed up and slow down

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::f64::consts::PI;
+use std::{f64::consts::PI, time::Duration};
 
 #[cfg(not(target_arch = "wasm32"))]
 use bevy::sprite_render::Wireframe2dConfig;
@@ -8,7 +8,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(SolarSystemPlugin)
-        .add_systems(Update,  toggle_wireframe.run_if(input_just_pressed(KeyCode::Space)))
+        .add_systems(Update,  ui_keyboard_controls_system)
         .run();
 }
 
@@ -19,7 +19,7 @@ fn add_planets(mut commands: Commands) {
 }
 
 fn move_celestial_body(time: Res<Time>, mut timer: ResMut<OrbitTimer>, mut query: Query<(&Name, &mut PolarPosition, &Orbit), With<CelestialBody>>) {
-    if timer.0.tick(time.delta()).just_finished() {
+    if timer.timer.tick(time.delta()).just_finished() {
         for (name, mut polar_position, orbit) in query.iter_mut() {
             // Noddy way to make big orbits go slower
             polar_position.0 += 1.0/orbit.0;
@@ -28,31 +28,87 @@ fn move_celestial_body(time: Res<Time>, mut timer: ResMut<OrbitTimer>, mut query
     }
 }
 
-fn keyboard_input_system(
+fn timer_keyboard_controls_system(
     keyboard_input: Res<ButtonInput<KeyCode>>,
-    key_input: Res<ButtonInput<Key>>,
+    // key_input: Res<ButtonInput<Key>>, // if you want a key that appears in multiple locations
+    ref mut timer: ResMut<OrbitTimer>,
 ) {
+    if keyboard_input.just_pressed(KeyCode::Space) {
+        println!("<INP> Toggle Pause");
+        timer.toggle_pause();
+    }
+
     if keyboard_input.just_pressed(KeyCode::Period) {
-        println!("speed uppppp")
+        println!("<INP> Speed Up");
+        timer.speed_up();
     }
 
     if keyboard_input.just_pressed(KeyCode::Comma) {
-        println!("slow downnnn")
+        println!("<Inp> Slow Down");
+        timer.slow_down();
     }
+}
 
+
+fn ui_keyboard_controls_system(
+    // keyboard_input: Res<ButtonInput<KeyCode>>, // if you want a single clickable key on the keyboard
+    key_input: Res<ButtonInput<Key>>,
+) {
     let key = Key::Character("?".into());
     if key_input.just_pressed(key.clone()) {
         println!("show helpppp");
     }
 }
 
-#[cfg(not(target_arch = "wasm32"))]
-fn toggle_wireframe(mut wireframe_config: ResMut<Wireframe2dConfig>) {
-    wireframe_config.global = !wireframe_config.global;
+#[derive(Resource)]
+struct OrbitTimer{
+    current_interval: usize,
+    times: [f32; 6], // tick times increasing in duration with index 
+    timer:  Timer
 }
 
-#[derive(Resource)]
-struct OrbitTimer(Timer);
+impl OrbitTimer {
+    fn slow_down(&mut self){
+        if self.current_interval == self.times.len()-1 {
+            return
+        }
+
+        self.current_interval += 1;
+        self.timer.set_duration(Duration::from_secs_f32( self.times[self.current_interval]));
+    }
+    
+    fn speed_up(&mut self) {
+        if self.current_interval == 0 {
+            return
+        }
+
+        self.current_interval -= 1;
+        self.timer.set_duration(Duration::from_secs_f32( self.times[self.current_interval]));
+    }
+
+    fn toggle_pause(&mut self){
+        if self.timer.is_paused() {
+            self.timer.unpause();
+        } else {
+            self.timer.pause();
+        }
+    }
+
+    // fn set_pause(&mut self, pause:bool){
+    //     self.paused = pause;
+    // }
+}
+
+fn new_orbit_timer() -> OrbitTimer{
+    let times = [0.05, 0.1, 0.25, 0.5, 1.0, 2.0];  // in seconds
+    let current_interval = times.len() - 1;
+
+    return OrbitTimer{
+        current_interval,
+        times,
+        timer: Timer::from_seconds(times[current_interval], TimerMode::Repeating)
+    }
+}
 
 #[derive(Component)]
 struct CelestialBody;
@@ -71,8 +127,8 @@ pub struct SolarSystemPlugin;
 
 impl Plugin for SolarSystemPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(OrbitTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
+        app.insert_resource(new_orbit_timer());
         app.add_systems(Startup, add_planets);
-        app.add_systems(Update,  (move_celestial_body,  keyboard_input_system));
+        app.add_systems(Update,  (move_celestial_body,  timer_keyboard_controls_system));
     }
 }
