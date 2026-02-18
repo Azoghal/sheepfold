064ad6035a529adb59b3ea9fc059b6ad9f29d085
Sam Beer <sam.beer208@gmail.com>
2026-02-18 21:02:30 +0000

    render a planet, somewhere, somehow

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,12 @@
 use std::{f64::consts::PI, time::Duration};
 
-#[cfg(not(target_arch = "wasm32"))]
-use bevy::sprite_render::Wireframe2dConfig;
-use bevy::{DefaultPlugins, app::{App, Plugin, Startup, Update}, ecs::{component::Component, query::With, resource::Resource, schedule::IntoScheduleConfigs, system::{Commands, Query, Res, ResMut}}, input::{ButtonInput, common_conditions::input_just_pressed, keyboard::{Key, KeyCode}}, time::{Time, Timer, TimerMode}};
+use bevy::{DefaultPlugins, app::{App, Plugin, Startup, Update}, asset::Assets, camera::Camera2d, color::Color, ecs::{component::Component, query::With, resource::Resource, system::{Commands, Query, Res, ResMut}}, input::{ButtonInput, keyboard::{Key, KeyCode}}, math::primitives::{Circle, ToRing}, mesh::{Mesh, Mesh2d}, sprite_render::{ColorMaterial, MeshMaterial2d}, time::{Time, Timer, TimerMode}, transform::components::Transform};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(SolarSystemPlugin)
-        .add_systems(Update,  ui_keyboard_controls_system)
         .run();
-}
-
-fn add_planets(mut commands: Commands) {
-    commands.spawn((CelestialBody, Name("Chesterton".to_string()), Orbit(400000.0), PolarPosition(0.0)));
-    commands.spawn((CelestialBody, Name("Abbey".to_string()), Orbit(600000.0), PolarPosition(0.5*PI)));
-    commands.spawn((CelestialBody, Name("Petersfield".to_string()), Orbit(800000.0), PolarPosition(1.5*PI)));
-}
-
-fn move_celestial_body(time: Res<Time>, mut timer: ResMut<OrbitTimer>, mut query: Query<(&Name, &mut PolarPosition, &Orbit), With<CelestialBody>>) {
-    if timer.timer.tick(time.delta()).just_finished() {
-        for (name, mut polar_position, orbit) in query.iter_mut() {
-            // Noddy way to make big orbits go slower
-            polar_position.0 += 1.0/orbit.0;
-            println!("Planet {} is now at {}/{} of its orbit.", name.0, polar_position.0, 2.0*PI);
-        }
-    }
 }
 
 fn timer_keyboard_controls_system(
@@ -129,6 +110,37 @@ impl Plugin for SolarSystemPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(new_orbit_timer());
         app.add_systems(Startup, add_planets);
-        app.add_systems(Update,  (move_celestial_body,  timer_keyboard_controls_system));
+        app.add_systems(Update,  (move_celestial_body,  timer_keyboard_controls_system, ui_keyboard_controls_system));
+    }
+}
+
+
+fn add_planets(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>) {
+
+    commands.spawn(Camera2d);
+
+    let colour = Color::hsl(144., 0.95, 0.7);
+    let shape = meshes.add(Circle::new(50.0).to_ring(5.));
+
+    commands.spawn((
+        CelestialBody, 
+        Name("Chesterton".to_string()),
+        Orbit(400000.0), 
+        PolarPosition(0.0),
+        Mesh2d(shape),
+        MeshMaterial2d(materials.add(colour)),
+        Transform::from_xyz(-500.0, -75.0, 0.0)
+    ));
+    commands.spawn((CelestialBody, Name("Abbey".to_string()), Orbit(600000.0), PolarPosition(0.5*PI)));
+    commands.spawn((CelestialBody, Name("Petersfield".to_string()), Orbit(800000.0), PolarPosition(1.5*PI)));
+}
+
+fn move_celestial_body(time: Res<Time>, mut timer: ResMut<OrbitTimer>, mut query: Query<(&Name, &mut PolarPosition, &Orbit), With<CelestialBody>>) {
+    if timer.timer.tick(time.delta()).just_finished() {
+        for (name, mut polar_position, orbit) in query.iter_mut() {
+            // Noddy way to make big orbits go slower
+            polar_position.0 += 1.0/orbit.0;
+            println!("Planet {} is now at {}/{} of its orbit.", name.0, polar_position.0, 2.0*PI);
+        }
     }
 }
