ebbd3faa89e66fb947cb3d353000b7bffca2ddbb
Sam Beer <sam.beer208@gmail.com>
2026-04-02 21:05:46 +0100

    working initial main menu plugin with display

--- src/main_menu/mod.rs
+++ src/main_menu/mod.rs
@@ -1,14 +1,7 @@
 use bevy::{
-    app::{App, Plugin, Startup},
-    ecs::system::Commands,
-    state::state_scoped::DespawnOnExit,
-    ui::{Node, PositionType, widget::Text},
-    utils::default,
+    app::{App, Plugin, Startup}, camera::{Camera, Camera2d, ClearColorConfig, Viewport}, color::Color, ecs::system::{Commands, Single}, state::state_scoped::DespawnOnExit, ui::{Node, PositionType, widget::Text}, utils::default, window::Window
 };
-use bevy_egui::{
-    EguiContexts, EguiPlugin, EguiPrimaryContextPass,
-    egui::{ Window},
-};
+use bevy_egui::{EguiContexts, EguiPlugin, EguiPrimaryContextPass};
 
 use crate::AppState;
 
@@ -22,7 +15,23 @@ impl Plugin for MainMenuPlugin {
     }
 }
 
-fn menu_setup(mut commands: Commands) {
+fn menu_setup(mut commands: Commands, window: Single<&Window>) {
+    let window_size = window.resolution.physical_size().as_vec2();
+
+    commands.spawn((
+        DespawnOnExit(AppState::MainMenu),
+        Camera2d,
+        Camera {
+            viewport: Some(Viewport {
+                physical_position: (window_size * 0.0).as_uvec2(),
+                physical_size: (window_size * 1.0).as_uvec2(),
+                ..default()
+            }),
+            clear_color: ClearColorConfig::Custom(Color::BLACK),
+            ..default()
+        },
+    ));
+
     commands.spawn((
         DespawnOnExit(AppState::MainMenu),
         Text::new("hello"),
@@ -36,7 +45,7 @@ fn menu_setup(mut commands: Commands) {
 fn main_menu_ui(mut contexts: EguiContexts) {
     match contexts.ctx_mut() {
         Ok(context) => {
-            Window::new("Debug").show(context, |ui| {
+            bevy_egui::egui::Window::new("Debug").show(context, |ui| {
                 ui.label("Sheepfold");
             });
         }
